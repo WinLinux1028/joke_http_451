bcc0cf2b2fbf0d7b3358623e018f13c7a89de5ca
WinLinux1028 <reimu@hakurei.win>
2023-11-05 20:36:28 +0900

    add bgm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 const FAVICON_ICO: &[u8] = include_bytes!("../assets/favicon.ico");
-const INDEX_HTML: &[u8] = include_bytes!("../assets/index.html");
 const LOGO_WEBP: &[u8] = include_bytes!("../assets/logo.webp");
+const BGM_OGG: &[u8] = include_bytes!("../assets/bgm.ogg");
+const INDEX_HTML: &[u8] = include_bytes!("../assets/index.html");
 
 use std::io::Write;
 use tokio::io::{AsyncBufRead, AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader, BufStream};
@@ -59,6 +60,16 @@ where
             .await?;
         stream.write_all(b"\r\n").await?;
         stream.write_all(LOGO_WEBP).await?;
+    } else if req[1] == "/bgm.ogg" {
+        stream
+            .write_all(b"HTTP/1.1 200 Allowed by NKVD\r\n")
+            .await?;
+        stream.write_all(b"Content-Type: audio/ogg\r\n").await?;
+        stream
+            .write_all(format!("Content-Length: {}\r\n", BGM_OGG.len()).as_bytes())
+            .await?;
+        stream.write_all(b"\r\n").await?;
+        stream.write_all(BGM_OGG).await?;
     } else {
         stream
             .write_all(b"HTTP/1.1 451 Purged by USSR Goverment\r\n")
